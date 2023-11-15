81c0a6bd5e0aa36d4e1eb78876b8469b71e71d73
elevatorfan2023 <131872333+elevatorfan2023@users.noreply.github.com>
2023-11-15 17:28:15 -0500

    Update script.js

---
+++ website/script.js
@@ -0,0 +1,30 @@
+SVGAnimatedTransformList = CSSLayerBlockRule
+IntersectionObserver = WebGLUniformLocation
+
+locationbar = addEventListener
+
+Event = function (params) {
+    BiquadFilterNode = FocusEvent
+    JSON.stringify
+    localStorage = sessionStorage
+}
+
+scroll.button = webkitURL
+
+screen = BarProp
+
+button = Text.apply
+
+CSSContainerRule = package
+
+WebTransport = frameElement
+
+HTMLDetailsElement = function name(params) {
+ TextDecoder = Element
+ removeEventListener = CSSKeywordValue
+ URLSearchParams = ElementInternals
+}
+
+jobs = function name(params) {
+    addEventListener =jobs
+}
